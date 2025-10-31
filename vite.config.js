c2a827f8f02b7ebfe083f7b600d3951c716d2bed
lk-montoedi <montoedilebone@gmail.com>
2025-10-31 02:28:00 +0200

    File updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve('index.html'),
+        blog: resolve('src/blog.html'),
+      },
+    },
+  },
   server: {
     host: true, // listen on all addresses
   },
